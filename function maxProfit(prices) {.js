bd90a43a9899247870e7a77c6e7e8fe5f2b92e2b
Brian Aston <astonb.dev@gmail.com>
2022-05-12 19:55:29 -0700

    Removing misplaced files

--- function maxProfit(prices) {.js
+++
@@ -1,9 +0,0 @@
-function maxProfit(prices) {
-  let minPrice = Infinity;
-  const maxProfits = []; // max you could make by selling on this day
-  for(let price of prices) {
-    minPrice = price < minPrice ? price : minPrice;
-    maxProfits.push(price - minPrice);
-  }
-  return Math.max(...maxProfits);
-}
